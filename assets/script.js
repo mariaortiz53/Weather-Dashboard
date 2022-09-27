13991f5f74e7ea230313bbd6510b508eceaed844
mariaortiz53 <106206330+mariaortiz53@users.noreply.github.com>
2022-09-26 22:42:17 -0500

    added starter code

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const apiKey = "6fb3bb46421e4f1e3582a79e7b9f1b21";
+const mainEl = ('#main');
+
+
+search.addEventListener("click"                                                                                                                                                                                                                                                                                                                   )
